d65a0447a82b471070b57056542089c1b16602cd
nasim1903 <nasimahmed0113@gmail.com>
2022-11-10 16:18:10 +0000

    Added Services fix

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 global using dotnet_prac.Models;
+using dotnet_prac.Services.CharacterServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
