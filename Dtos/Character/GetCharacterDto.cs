530f8b0f56fbad7e1dc032f623892ed2f8dcb3fc
nasim1903 <nasimahmed0113@gmail.com>
2022-11-15 14:50:41 +0000

    Installed AutoMapper and DTO files

---
+++ Dtos/Character/GetCharacterDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_prac.Dtos.Character
+{
+    public class GetCharacterDto
+    {
+        public static int idNum = 0;
+        public int id { get; set; } = idNum;
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+
+        public GetCharacterDto()
+        {
+            idNum++;
+        }
+    }
+}
