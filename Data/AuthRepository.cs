9da7c3a35ed926aa0662b14bce31b9cf6b2b5b5a
nasim1903 <nasimahmed0113@gmail.com>
2022-11-23 20:11:25 +0000

    Added password hashing

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -7,19 +7,40 @@ namespace dotnet_prac.Data
 {
     public class AuthRepository : iAuthRepository
     {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            CreateHashPassword(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            var response = new ServiceResponse<int>();
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+            response.Data = user.id;
+            return response;
         }
 
         public Task<bool> UserExist(string username)
         {
             throw new NotImplementedException();
+        }
+        private void CreateHashPassword(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512()){
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
